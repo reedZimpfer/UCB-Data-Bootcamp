3de06a03e508491b389e3cc0db47bda7e7ce286e
reedZimpfer <119377309+reedZimpfer@users.noreply.github.com>
2022-12-13 18:25:37 -0800

    why
    commit

---
+++ python/1/Class_1_Activities/04-Stu_HelloVariableWorld/hellovarworld.py
@@ -0,0 +1,16 @@
+
+name = "Santa"
+
+country = "Argentina"
+
+age = 27
+
+hourly_wage = 15
+
+satisfied = True
+
+daily_wage = hourly_wage * 8
+
+print (name + " " + country + " " + str(age) + " " + str(hourly_wage))
+
+print (f"This fellow {name} from {country} is {age}yrs old and has a hourly wage")
